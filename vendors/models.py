4e98f27e682a756d9e59b49595b6ae3216ee1941
Santosh <80888066+i-santosh@users.noreply.github.com>
2024-05-02 12:51:33 +0530

    Adding Vendor Profile Management

--- vendors/models.py
+++ vendors/models.py
@@ -5,7 +5,7 @@ class Vendor(models.Model):
     name = models.CharField(max_length=100)
     contact_details = models.TextField()
     address = models.TextField()
-    vendor_code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    vendor_code = models.CharField(max_length=10, unique=True)
     on_time_delivery_rate = models.FloatField(blank=True, default=0.0)
     quality_rating_avg = models.FloatField(blank=True, default=0.0)
     average_response_time = models.FloatField(blank=True, default=0.0)
